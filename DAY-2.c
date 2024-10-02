355319b0271bd3d27838be652c2fa8896a421134
dhruvji420 <djhruv4818@gmail.com>
2024-10-02 22:47:21 +0530

    DAY-2

---
+++ DAY-2.c
@@ -0,0 +1,9 @@
+/*code for finding the volume of sphere by giving radius*/
+#include<stdio.h>
+int main(){
+    float radius ;
+    printf("enter radius =");
+    scanf("%f",&radius);
+    printf("volume of sphere is = %f",4*3.1415*radius*radius*radius/3);
+    return 0;
+} 
