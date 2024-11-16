539f80b1274a06f833ae6426b54d4d98f2015014
dhruvji420 <djhruv4818@gmail.com>
2024-11-16 22:45:20 +0530

    DAY-45

---
+++ DAY-45PRESENTS.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int n;
+	    scanf("%d",&n);
+	    
+	        printf("%d\n",n-(n/5));
+	    
+	}
+	
+	return 0;
+}
+
